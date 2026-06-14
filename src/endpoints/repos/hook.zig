66448b0943a8d2973eb5d5abd254af143f09533d
Gregory Mullen <_@gr.ht>
2026-06-14 16:35:40 -0700

    parse and debug hook calls

--- src/endpoints/repos/hook.zig
+++ src/endpoints/repos/hook.zig
@@ -10,6 +10,38 @@ const UpdateData = struct {
     newrev: []const u8,
 };
 
+pub const ZonConf = struct {
+    srctree: ?struct {
+        docs: ?[]const u8 = null,
+        tagged: ?[]const u8 = null,
+        nightly: ?[]const u8 = null,
+    } = null,
+};
+
+pub const AfterParty = struct {
+    invites: std.ArrayList(Invite),
+
+    pub fn invite(party: *AfterParty, name: []const u8, ptr: *anyopaque) !void {
+        std.debug.print("{s} invited to after party\n", .{name});
+        _ = party;
+        _ = ptr;
+        //try party.invites.appendBounded(.{ .ptr = ptr });
+    }
+
+    pub const Invite = struct {
+        name: []const u8 = &.{},
+        ptr: *anyopaque,
+    };
+};
+
+pub const CI = struct {
+    name: []const u8,
+};
+
+var after_party: AfterParty = .{
+    .invites = .empty,
+};
+
 fn update(f: *Frame) Router.Error!void {
     const rd = RouteData.init(f.uri) orelse return error.Unrouteable;
     const vis: repos.Visibility.Select = if (f.user) |_| .all else .public_only;
@@ -17,7 +49,30 @@ fn update(f: *Frame) Router.Error!void {
     repo.loadData(f.alloc, f.io) catch return error.ServerFault;
 
     const update_data = f.request.data.query.validate(UpdateData) catch return error.DataInvalid;
-    std.debug.print("{}\n", .{update_data});
+    const commit = repo.commit(.init(update_data.newrev), f.alloc, f.io) catch unreachable;
+    const tree = commit.loadTree(&repo, f.alloc, f.io) catch unreachable;
+    var blb: git.Blob = undefined;
+    for (tree.blobs) |b| {
+        blb = b;
+        if (std.mem.eql(u8, blb.name, "build.zig.zon")) break;
+        // TODO add ffs support
+        //if (std.mem.eql(u8, blb.name, "build.ffs")) break;
+    } else return f.sendHTML(.ok, "plz no 502");
+
+    var resolve = repo.loadBlob(blb.sha, f.alloc, f.io) catch return error.ServerFault;
+    if (!resolve.isFile()) return {};
+    const data = try f.alloc.dupeSentinel(u8, resolve.data.?, 0);
+    var diag: std.zon.parse.Diagnostics = .{};
+    if (std.zon.parse.fromSliceAlloc(ZonConf, f.alloc, data, &diag, .{ .ignore_unknown_fields = true })) |zon| {
+        if (zon.srctree) |srctree| {
+            var ci: CI = .{ .name = &.{} };
+            if (srctree.docs) |d| after_party.invite(d, &ci) catch unreachable;
+            if (srctree.tagged) |t| after_party.invite(t, &ci) catch unreachable;
+            if (srctree.nightly) |n| after_party.invite(n, &ci) catch unreachable;
+        }
+    } else |err| std.debug.print("err {}\n", .{err});
+
+    return f.sendHTML(.ok, "plz no 502");
 }
 
 const std = @import("std");
@@ -25,7 +80,7 @@ const std = @import("std");
 const repos = @import("../../repos.zig");
 const RepoEndpoint = @import("../repos.zig");
 const RouteData = RepoEndpoint.RepoRouter;
-//const git = @import("../../git.zig");
+const git = @import("../../git.zig");
 
 const verse = @import("verse");
 const Frame = verse.Frame;
